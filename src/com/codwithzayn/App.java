770344be07a9176b114495787d7f92487846c6f4
zaladin1 <125614120+zaladin1@users.noreply.github.com>
2023-02-24 15:36:41 -0700

    Update App.java

--- src/com/codwithzayn/App.java
+++ src/com/codwithzayn/App.java
@@ -10,24 +10,31 @@ import javax.print.DocFlavor.STRING;
 public class App {
 
     public static void main(String[] args) {
-    
+
+    // Initiate a scanner and collect the user input to a word.
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a word");
+
+     // the inputted word is stored in the variable word   
         String word = scanner.next();
         String reverse ="";
         char ch;
 
         
        
-      
+      // The following for loop, returns the inverse of the inputted word.  This inversed word is stored into the reverse variable.
         for (int i = word.length(); i >=0 ; i--) {
             ch = word.charAt(i);
             reverse = reverse + ch;
         }
 
+
         System.out.println(word);
         System.out.println(reverse);
 
+
+        // This is an if else statement, saying that if both the inputted word and the reversed word are the same, then it is a palindrome, otherwise it is not a palindrome. 
+        // The program is not case sensitive.
         if (word.toLowerCase().equals(reverse.toLowerCase())) {
             System.out.println("Input string is palindrome");
         } else {
